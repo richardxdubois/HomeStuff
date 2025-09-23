b2c2999905c2f1fde416fe840326600ec907a01d
Richard Dubois <richard.x.dubois@gmail.com>
2025-09-23 14:17:34 -0700

    handling Photoshop bug

--- python/correlate_photo_gps.py
+++ python/correlate_photo_gps.py
@@ -37,8 +37,11 @@ class correlate_photo_gps():
         self.photo_dir = data['photo_dir']
         self.gpx_list = data["gpx_list"]
         self.photo_list = data["photo_list"]
+        self.do_all_photos = data["do_all_photos"]
 
         self.kml = simplekml.Kml()
+
+    def correlate_gpx_photos(self):
 
         self.lat = []
         self.lon = []
@@ -50,41 +53,68 @@ class correlate_photo_gps():
         self.current_photo = photo()
         self.photo_properties = {}
 
+        if len(self.photo_list) == 0:
+            path = Path(self.photo_dir)
+            p_list = list(path.glob("*.[jJ][pP][gG]"))  # case-insensitive
+
+            posix_p_list = np.sort([Path(file.as_posix()).name for file in p_list])
+        else:
+            posix_p_list = self.photo_list
+
+        got_gps = "got_gps"
+        self.photo_properties.setdefault(got_gps, {})
+
+        print("Processing photos with gps already")
+        for p in posix_p_list:
+            p_loc = self.photo_dir + p
+            p_name = str(p)
+            self.current_photo.get_image(p_loc)
+            if self.current_photo.latitude is None:
+                continue
+
+            self.photo_properties[got_gps].setdefault(p_name, {})
+            self.photo_properties[got_gps][p_name]["name"] = p
+            self.photo_properties[got_gps][p_name]["lat"] = self.current_photo.latitude
+            self.photo_properties[got_gps][p_name]["lon"] = self.current_photo.longitude
+            self.photo_properties[got_gps][p_name]["time"] = self.current_photo.create_date
+            self.photo_properties[got_gps][p_name]["filespec"] = p_loc
+
         for g in self.gpx_list:
 
-            self.photo_properties.setdefault(g, {})
+            g_name = str(Path(g).name)
+            print("Processing gpx file ", g_name)
+
+            self.photo_properties.setdefault(g_name, {})
 
             gpx_file = self.gpx_dir + g
             with (open(gpx_file, 'r') as g_file):
                 gpx = gpxpy.parse(g_file)
 
-            self.process_gpx(gpx=gpx)
+            for pl in posix_p_list:
 
-            if len(self.photo_list) == 0:
-                path = Path(self.photo_dir)
-                p_list = list(path.glob('*.JPG'))  # '*/' for non-recursive
-
-                posix_p_list = np.sort([Path(file.as_posix()).name for file in p_list])
-            else:
-                posix_p_list = self.photo_list
-
-            for p in posix_p_list:
+                p = str(pl)
+                p_name = str(Path(p).name)
 
                 photo_file = self.photo_dir + p
 
                 self.current_photo.get_image(photo_file)
+
+                if self.current_photo.latitude is not None:
+                    continue
+
+                rc = self.process_gpx(gpx=gpx)
+
                 timezone_name = self.tf.timezone_at(lat=self.lat[0], lng=self.lon[0])
                 timezone = pytz.timezone(timezone_name)
-                t_photo = timezone.localize(self.current_photo.create_date)
-                time_tz = t_photo.astimezone(timezone)
+                time_tz = self.current_photo.create_date.astimezone(timezone)
 
                 if time_tz < self.time[0] or time_tz > self.time[-1]:
-                    print("Photo not taken during gps route")
-                    break
+                    #print("Photo not taken during gps route")
+                    continue
 
                 t_diff = []
                 for i in range(len(self.time)):
-                    t_diff.append((self.time[i] - t_photo).seconds)
+                    t_diff.append((self.time[i] - time_tz).seconds)
 
                 closest_to_zero = 100000
                 closest_index = -1
@@ -92,12 +122,20 @@ class correlate_photo_gps():
                     if t_diff[i] < closest_to_zero:
                         closest_to_zero = t_diff[i]
                         closest_index = i
+                latitude = self.lat[closest_index]
+                longitude = self.lon[closest_index]
+                time = self.time[closest_index]
 
-                self.photo_properties[g].setdefault(p, {})
-                self.photo_properties[g][p]["name"] = p
-                self.photo_properties[g][p]["lat"] = self.lat[closest_index]
-                self.photo_properties[g][p]["lon"] = self.lon[closest_index]
-                self.photo_properties[g][p]["time"] = self.time[closest_index]
+                self.photo_properties[g_name].setdefault(p_name, {})
+                self.photo_properties[g_name][p_name]["name"] = p
+                self.photo_properties[g_name][p_name]["lat"] = latitude
+                self.photo_properties[g_name][p_name]["lon"] = longitude
+                self.photo_properties[g_name][p_name]["time"] = time
+                self.photo_properties[g_name][p_name]["filespec"] = photo_file
+
+        return self.photo_properties
+
+    def print_photos_gps(self):
 
         print("  Name          LAT        LON              Time")
         for g in self.gpx_list:
@@ -133,6 +171,7 @@ class correlate_photo_gps():
                     timezone = pytz.timezone(timezone_name)
                     time_tz = point.time.astimezone(timezone)
                     self.time.append(time_tz)
+                    #self.time.append(point.time)
 
                     distance = 0.
 
@@ -153,7 +192,7 @@ class photo():
     def __init__(self, name=None):
 
         self.name = name
-        self.caption = ""
+        self.caption = None
         self.create_date = ""
         self.camera = ""
         self.file_location = ""
@@ -164,6 +203,14 @@ class photo():
 
         self.image_obj = ""
 
+        self.latitude = None
+        self.longitude = None
+
+    def dms_to_dd(self, deg, minutes, seconds, hemi):
+        sign = -1 if hemi.upper() in ('S', 'W') else 1
+
+        return float(sign * (abs(deg) + minutes / 60 + seconds / 3600))
+
     def get_image(self, file_location=None):
 
         self.file_location = file_location
@@ -171,17 +218,48 @@ class photo():
         self.image_obj = Image.open(file_location)
         self.image_size = self.image_obj.size
 
+        iptc = IptcImagePlugin.getiptcinfo(self.image_obj)
+        self.caption = None
+
+        try:
+            self.caption = iptc[(2, 120)].decode("utf-8")
+        except:
+            try:
+                self.caption = iptc[(2, 120)].decode("cp1252")
+            except:
+                #print("Failed to get caption", file_location, iptc)
+                pass
+        self.longitude = None
+        self.latitude = None
+
         # https://hhsprings.bitbucket.io/docs/programming/examples/python/PIL/ExifTags.html
         _TAGS_r = dict(((v, k) for k, v in TAGS.items()))
         exifd = self.image_obj._getexif()
         keys = list(exifd.keys())
         try:
             t = exifd[36868]
-        except KeyError:
-            t = exifd[36867]
+        except:
+            try:
+                t = exifd[36867]
+            except:
+                t = exifd[306]    # unusual location used by Photoshop saving HEIC files as JPG
+                pass
 
         self.create_date = datetime.strptime(t, '%Y:%m:%d %H:%M:%S')
-        self.camera = exifd[272]
+        try:
+            self.camera = exifd[272]
+        except:
+            self.camera = "Unknown camera"
+            pass
+        try:
+            gpsinfo = exifd[_TAGS_r["GPSInfo"]]
+
+            self.latitude = self.dms_to_dd(deg=gpsinfo[2][0], minutes=gpsinfo[2][1],
+                                 seconds=gpsinfo[2][2], hemi=gpsinfo[1])
+            self.longitude = self.dms_to_dd(deg=gpsinfo[4][0], minutes=gpsinfo[4][1],
+                                  seconds=gpsinfo[4][2], hemi=gpsinfo[3])
+        except:
+            pass
 
 
 if __name__ == "__main__":
@@ -195,3 +273,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     a = correlate_photo_gps(config=args.app_config)
+
+    p = a.correlate_gpx_photos()
+    rc = a.print_photos_gps()
